f2bc7be7aa637f0facce1ffeb018f160fe351840
selvin-infocusp <selvin@infocusp.com>
2024-12-05 17:09:25 +0530

    move file.

--- data/init_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Download kaggle dataset.
-kaggle competitions download -c cassava-leaf-disease-classification
-
-# Unzip downloaded file.
-unzip cassava-leaf-disease-classification.zip
